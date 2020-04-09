bf868f493036325549a58ceb3073f546a27baea6
Tony Fast <tony.fast@gmail.com>
2020-04-09 12:14:45 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
 master_doc = 'index'
-extensions = 'autoapi.extension sphinx.ext.napoleon'.split()
+extensions = 'nbsphinx autoapi.extension sphinx.ext.napoleon'.split()
 
 autoapi_type = 'python'
 autoapi_dirs = ['.']
