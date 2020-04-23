bfa29cf61be8aee77042df6df83776cbe7d3bea9
Tony Fast <tony.fast@gmail.com>
2020-04-23 09:33:23 -0400

    Update __init__.py

--- wtypes/__init__.py
+++ wtypes/__init__.py
@@ -22,15 +22,15 @@ Todo
 __version__ = "0.0.2"
 
 from .spec import *  # isort:skip
+from . import validation  # isort:skip
 from . import base, combining_types, dataclass, python_types, utils
 from .combining_types import *
 from .dataclass import *
 from .python_types import *
 from .string_formats import *
-from .validate import *
+from .validation import validate
 
 from .utils import *  # isort:skip
 from .base import *  # isort:skip
 from . import examples  # isort:skip
-
 from . import evented  # isort:skip
